388b4e4efc8717951dcd59389f73cde81fec5122
katew <ciangmiang@gmail.com>
2018-04-25 14:09:25 +0700

    Initial commit

---
+++ app/src/main/java/katew/mtp/ac/th/showmtp/MainActivity.java
@@ -0,0 +1,24 @@
+package katew.mtp.ac.th.showmtp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import katew.mtp.ac.th.showmtp.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Add Fragment
+
+        if (savedInstanceState==null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+        }
+
+    }   // Main Method
+}   // Main Class
