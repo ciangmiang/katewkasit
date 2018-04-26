698c9617dfc3de59c1c077909b0a96723861034c
katew <ciangmiang@gmail.com>
2018-04-26 13:06:38 +0700

    connect db 25/04/61

---
+++ app/src/main/java/katew/mtp/ac/th/showmtp/Utility/MyConstant.java
@@ -0,0 +1,11 @@
+package katew.mtp.ac.th.showmtp.Utility;
+
+public class MyConstant {
+
+    private String UrlAddUser="http://androidthai.in.th/bru/addUser.php";
+
+
+    public String getUrlAddUser() {
+        return UrlAddUser;
+    }
+}
